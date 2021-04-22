493d73bbb7931d3681db39737116ba97d6b12ba8
ItA <32574079+alexriffaud@users.noreply.github.com>
2021-04-22 16:21:47 +0200

    Add initial project

---
+++ aps-game-api/src/main/java/com/esimed/apsgameapi/ApsGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.esimed.apsgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsGameApiApplication.class, args);
+	}
+
+}
---
+++ aps-game-api/src/test/java/com/esimed/apsgameapi/ApsGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esimed.apsgameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
